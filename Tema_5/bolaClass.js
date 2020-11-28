470c91cd75399549d58f94a1972f25ce4794883a
MichaelBurri <miguel_burri@hotmail.com>
2020-11-28 21:06:48 +0100

    Ummm

--- Tema_5/bolaClass.js
+++ Tema_5/bolaClass.js
@@ -1,33 +1,50 @@
 class Bola{
-    constructor(){
+    constructor(svg, id){                
         this.id=id;
         this.cx=aleatorio(0,500);
         this.cy=aleatorio(0,500);
-        this.r=aleatorio(0,50);
-        this.fill=rgb(aleatorio(0,255),aleatorio(0,255),aleatorio(0,255));
+        this.r=aleatorio(1,50);
+        this.fill="rgb("+aleatorio(0,255)+","+aleatorio(0,255)+","+aleatorio(0,255)+")";
+        //
+        this.circ=document.createElementNS("http://www.w3.org/2000/svg","circle");
+        this.circ.setAttributeNS(null, 'id', "circ"+this.id);
+        this.circ.setAttributeNS(null, 'cx', this.cx);
+        this.circ.setAttributeNS(null, 'cy', this.cy);
+        this.circ.setAttributeNS(null, 'r', this.r);
+        this.circ.setAttributeNS(null, 'fill', this.fill);
+        document.getElementById(svg).appendChild(this.circ);
     }
-
-
-}
-window.onload(){
-    crear();
-}
-
-function crear(){
-    let circ=document.createElementNS("http://www.w3.org/2000/svg","circle");
-    circ.setAttributeNS(null, 'id', 1);
-    circ.setAttributeNS(null, 'cx', aleatorio(0,500));
-    circ.setAttributeNS(null, 'cy', aleatorio(0,500));
-    circ.setAttributeNS(null, 'r', aleatorio(0,50));
-    circ.setAttributeNS(null, 'stroke', 'black');
-    circ.setAttributeNS(null, 'stroke-width',aleatorio(0,4));
-    circ.setAttributeNS(null, 'fill', (aleatorio(0,255),aleatorio(0,255),aleatorio(0,255)));
     
-    document.getElementById("Eseuvege").appendChild(circ);
+    animar(){
+        bola=this.circ;
+        bola.setAttributeNS(null, 'cx', 600);
+        bola.setAttributeNS(null, 'cy', 500);
+    }
 }
+
+var bolas=[];
+for (let i = 0; i < 5; i++) {
+   let bola=new Bola("svg",i);
+   bolas.push(bola);
+    
+}
+ function animarTodas() {
+    for (let i = 0; i < 5; i++)
+        bolas[i].setAttributeNS(null, 'cx', bolas[i].circ.cx+5);
+        bolas[i].setAttributeNS(null, 'cy', 500);
+} 
     
 
+
+
+
+//let bola1=new Bola("svg",1);
 function aleatorio(num1,num2){
     return parseInt(Math.random()*(num2-num1)+num1);
 }
+/* window.onload=()=>{
+
+    setInterval(animarTodas,20);
+    
+}; */
         
